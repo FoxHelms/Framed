98b8f346bb92936dbc19f009a4fcaa61656b9a33
FoxHelms <113215990+FoxHelms@users.noreply.github.com>
2023-05-18 23:57:57 -0400

    First draft
    This code loops through photo options as you try to guess shot type, giving you buttons to go back to all images.

---
+++ variables.js
@@ -0,0 +1,9 @@
+var imgInd = 1;
+var fileName = '';
+var guesses = 0;
+
+const shots = {
+    "wide shot": ["wideshot1.jpg", "wideshot2.jpg"],
+    "medium shot": ["medshot1.jpg","medshot2.jpg"],
+    "close up": ["closeup1.jpg","closeup2.jpg"]
+}
