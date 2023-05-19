f8800758a6f467184efa9ff37dbd8cbd83484af1
FoxHelms <113215990+FoxHelms@users.noreply.github.com>
2023-05-19 17:45:15 -0400

    package update
    new files and images

---
+++ variables.js
@@ -0,0 +1,9 @@
+var imgInd = 1;
+var fileName = '';
+var guesses = 0;
+
+const shots = {
+    "wide": ["wide1.jpg", "wide2.jpg"],
+    "medium": ["medium1.jpg","medium2.jpg"],
+    "close up": ["closeup1.jpg","closeup2.jpg"]
+}
