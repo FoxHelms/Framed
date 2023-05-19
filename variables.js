b479ff722388cb7cbdf5b0d7665d580f5e8209d0
FoxHelms <113215990+FoxHelms@users.noreply.github.com>
2023-05-19 18:17:19 -0400

    added more images and ecu answer exception

--- variables.js
+++ variables.js
@@ -5,5 +5,9 @@ var guesses = 0;
 const shots = {
     "wide": ["wide1.jpg", "wide2.jpg"],
     "medium": ["medium1.jpg","medium2.jpg"],
-    "close up": ["closeup1.jpg","closeup2.jpg"]
+    "close up": ["closeup1.jpg","closeup2.jpg"],
+    "cowboy": ["cowboy1","cowboy2","cowboy3"],
+    "extreme close up": ["extremecloseup1","extremecloseup2","extremecloseup3"],
+    "low": [""],
+    "dutch": [""]
 }
